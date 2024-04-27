5545c20a2d20fcaa8c2b08e84a9d7f6ae03ca521
Gieted <gieted@proton.me>
2024-04-27 17:41:03 +0200

    Add chatgpt

--- src/lib/chatgpt.ts
+++ src/lib/chatgpt.ts
@@ -1,26 +1,81 @@
-import axios from 'axios';
+import axios, {RawAxiosRequestHeaders} from 'axios';
 
-type message = {
-    role: 'user' | 'system',
+type Message = {
+    role: 'user' | 'system' | 'assistant',
     content: string,
 }
 
-export class ChatGpt {
-    private messages: message[] = [];
+type Question = {
+    question: string,
+    answers: string[],
+    correctAnswer: number,
+}
 
-    async chat(message: string): Promise<string> {
+type Choice = {
+    finish_reason: string,
+    message: Message,
+}
+
+export async function generateQuiz(topic: string): Promise<Question[]> {
+    let retries = 0;
+    while (true) {
+        const maxRetries = 10;
+        if (++retries > maxRetries) throw 'Failed to generate quiz';
+        console.log(`Generating quiz for topic (${retries}/${maxRetries}): ${topic}`);
+        const chatGpt = new ChatGpt();
+        chatGpt.messages.push({
+            role: 'system',
+            content: `
+            Jesteś botem generującym quizy. Użytkownik poda ci temat, a ty wygenerujesz quiz w postaci JSON.
+            Każde pytanie powinno posiadać 4 możliwe odpowiedzi oraz poprawną odpowiedź.
+            Każdy quiz powinien mieć dokładnie 10 pytań. Upewnij się, że twoja odpowiedź jest poprawnym JSONem.
+            Np. dla tematu "Zasady BHP", przykładowa odpowiedź to: 
+            [
+                {
+                    "question": "Jakie są główne cele przestrzegania zasad BHP?",
+                    "answers": [
+                        "Ochrona zdrowia i życia pracowników",
+                        "Ochrona mienia",
+                        "Ochrona środowiska naturalnego",
+                        "Wszystkie powyższe",
+                    ],
+                    "correctAnswer": 3
+                }
+            ]`
+        });
+        const response = await chatGpt.sendMessage(`Temat quizu: ${topic}`);
+        console.debug(`Response was: ${response.message.content}`);
+        const match = /(\[[\s\S]*])/g.exec(response.message.content);
+        const json = match?.[1];
+        console.debug(`Extracted JSON: ${json}`);
+        if (!json) {
+            console.debug('Response did not contain JSON.');
+            continue;
+        }
+        return JSON.parse(json);
+    }
+}
+
+export class ChatGpt {
+    messages: Message[] = [];
+
+    async sendMessage(message: string): Promise<Choice> {
         this.messages.push({role: 'user', content: message});
-        const response = await axios.post('https://https://training.nerdbord.io/api/v1/openai/chat/completions', {
-            'model': 'gpt-4-turbo',
+        console.log(this.messages);
+        const response = await axios.post('https://training.nerdbord.io/api/v1/openai/chat/completions', {
             'messages': this.messages,
         }, {
             headers: {
-                Authorization: '',
-            }
+                'Content-Type': 'application/json',
+                Authorization: '0fb37792bba9668ad9f53ffc5b2624393f7886ff3bb764f243b1c7a838d98364406b3a3e481c8857f77f80d4539c31c8d1e248447a8c8a9642cb6b12128eafc9',
+            } as RawAxiosRequestHeaders,
         });
 
         console.log(response.data);
 
-        return response.data;
+        const choice = response.data.choices[0] as Choice;
+        this.messages.push(choice.message);
+
+        return choice;
     }
 }
