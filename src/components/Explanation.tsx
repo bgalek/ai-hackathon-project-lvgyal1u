856580fdee57491cc30c581f643682888e1cbbcd
Bartosz Gałek <bartosz@galek.com.pl>
2024-04-28 12:05:08 +0200

    proper export

--- src/components/Explanation.tsx
+++ src/components/Explanation.tsx
@@ -7,7 +7,7 @@ export const Example = () => {
 
     return (
         <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="relative z-10">
+            <Dialog as="div" className="relative z-10" onClose={console.log}>
                 <Transition.Child
                     as={Fragment}
                     enter="ease-out duration-300"
